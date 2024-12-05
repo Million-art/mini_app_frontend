d26a11d228bb4f399cdaaca5a3d4d2bd70ac25b4
Kirubel Gizaw <meetkirubel@gmail.com>
2024-12-05 22:49:37 +0300

    daily

--- src/screens/Daily.tsx
+++ src/screens/Daily.tsx
@@ -1,7 +1,14 @@
+import { FaCalendarCheck } from "react-icons/fa"
 
 const Daily = () => {
   return (
-    <div>Daily</div>
+    <div className="text-white">
+      <div className="flex items-center justify-center py-10">
+          <div className="rounded-full p-4">
+              <FaCalendarCheck  className="w-28 h-28 "/>
+          </div>
+      </div>
+    </div>
   )
 }
 
