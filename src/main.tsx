8475cc13a7e6852c7a6e23e5102f5c96e212edb3
million-art <millionmulugeta09@gmail.com>
2024-12-11 11:31:16 +0300

    ton connect ui

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,8 @@ postEvent('web_app_setup_closing_behavior', { need_confirmation: true });
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TonConnectUIProvider
-      manifestUrl="https://red-urgent-damselfly-771.mypinata.cloud/files/bafkreifk3ef4hrbgdtrp5zqtqu6ba3yf4chirskvzvbkj3pfqeamrl7q5i?X-Algorithm=PINATA1&X-Date=1733899842&X-Expires=30&X-Method=GET&X-Signature=35e22f6873d02c27c68f580ee2ee5680f85cef643eda405bad0c299e3ca845b9"
-    >
+      manifestUrl="https://mini-app-frontend-bu51.vercel.app/"
+      >
       <Provider store={store}>
         <App />
       </Provider>
